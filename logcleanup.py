27ee2867abe316ef85e9649d5c2135ef8cc4aeff
gnuub <sookie_sayprasith@yahoo.com>
2018-10-16 00:31:57 -0400

    created sample loop to check for 'delete' in logdir.

--- logcleanup.py
+++ logcleanup.py
@@ -3,3 +3,17 @@
 """
 Program to remove files that match stem and extension.
 """
+
+from os import listdir
+
+
+def main():
+    logdir = 'C:\\Users\\sookie\\Projects\\Samples\\logs'
+
+    for fsobject in listdir(logdir):
+        if 'delete' in fsobject:
+            print(fsobject)
+
+
+if __name__ == '__main__':
+    main()
