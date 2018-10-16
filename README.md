96112c131bf00a3ff6df9553ecd0cc3d3726c54d
Sayprasith, Sooksun (CCI-Atlanta) <Sooksun.Sayprasith@cox.com>
2018-10-16 15:17:14 -0400

    replace static search of 'delete' with a regex search for patterns matching delete while ignoring case.
    Updated README.md to note what I'm practicing.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # logcleanup
 Python program to remove files based on stem and extension of filename.
+My personal project to cleanup my tmux logs as well as practice using the
+standard python library and regex to find something and do something with
+the results.
--- logcleanup.py
+++ logcleanup.py
@@ -4,16 +4,20 @@
 Program to remove files that match stem and extension.
 """
 
+import re
 from os import listdir
 
 
 def main():
-    logdir = 'C:\\Users\\sookie\\Projects\\Samples\\logs'
+    #dirobject = r'C:\Users\sookie\Projects\Samples\logs'
+    diroject = r'C:\Users\ssaypras\projects\Sample'
 
-    for fsobject in listdir(logdir):
-        if 'delete' in fsobject:
-            print(fsobject)
+    pattern = r'delete'
+    regex = re.compile(pattern, re.IGNORECASE)
 
+    for fnobject in listdir(diroject):
+        for match in regex.finditer(fnobject):
+            print(fnobject)
 
 if __name__ == '__main__':
     main()
