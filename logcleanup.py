4f943ae68557c958314da63cb33834d33de858c5
Sayprasith, Sooksun (CCI-Atlanta) <Sooksun.Sayprasith@cox.com>
2018-10-18 23:33:18 -0400

    Added argparse to accept command line arguments for directory and keyword filtering. Removed debugging comments. Removed static entries for directory and pattern to parse.

--- logcleanup.py
+++ logcleanup.py
@@ -1,25 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 """
 Program to remove files that match regex in given directory.
 """
 
 import re
+import argparse
 from os import listdir, remove
 from sys import platform
 
 def main():
-    dirobject = r'C:\Users\ssaypras\projects\Sample'
 
-    pattern = r'delete'
-    regex = re.compile(pattern, re.IGNORECASE)
+    parser = argparse.ArgumentParser(description='Program to remove files that match regex in given directory')
+    parser.add_argument('-d', '--directory', dest='directory', help='Directory to Clean', required=True)
+    parser.add_argument('-k', '--keyword', dest='keyword', help='Keyword search in filename to delete', required=True)
+    args = parser.parse_args()
+
+    dirobject = args.directory
+    regex = re.compile(args.keyword, re.IGNORECASE)
 
     for fnobject in listdir(dirobject):
         for match in regex.finditer(fnobject):
-            if platform == 'win32'
+            if platform.startswith('win32'):
                 file = dirobject + '\\' + fnobject
                 remove(file)
-            elif platform == 'linux'
+            elif platform.startswith('linux'):
                 file = dirobject + '/' + fnobject
                 remove(file)
             else:
