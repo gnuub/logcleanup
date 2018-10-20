78ce7f13821f19a1d284380b5e151205a7d7ac4f
Sayprasith, Sooksun (CCI-Atlanta) <Sooksun.Sayprasith@cox.com>
2018-10-20 13:12:49 -0400

    Updated with correct doc string notation and corrected comment notations.

--- logcleanup.py
+++ logcleanup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-"""
-logcleanup.py is a script to remove files in a directory that match a case insensitive pattern given through command
+"""logcleanup.py is a script to remove files in a directory that match a case insensitive pattern given through command
  line arguments.
 """
 
@@ -11,28 +10,28 @@ from os import listdir, remove
 from sys import platform
 
 def main():
-    "Create argument parser and expectation for arguments: directory and keyword to search"
+    #Create argument parser and expectation for arguments: directory and keyword to search
     parser = argparse.ArgumentParser(description='Program to remove files that match regex in given directory')
     parser.add_argument('-d', '--directory', dest='directory', help='Directory to Clean', required=True)
     parser.add_argument('-k', '--keyword', dest='keyword', help='Keyword search in filename to delete', required=True)
     args = parser.parse_args()
 
-    "Set global variables for directory and pattern from command arguments"
+    #Set global variables for directory and pattern from command arguments
     dirobject = args.directory
     regex = re.compile(args.keyword, re.IGNORECASE)
 
-    "Loop through each file in directory to see if filename matches pattern, if so remove it"
+    #Loop through each file in directory to see if filename matches pattern, if so remove it
     for fnobject in listdir(dirobject):
         for match in regex.finditer(fnobject):
-            "Check if is a Windows host"
+            #Check if is a Windows host
             if platform.startswith('win32'):
                 file = dirobject + '\\' + fnobject
                 remove(file)
-            "Check if is a Linux host"
+            #Check if is a Linux host
             elif platform.startswith('linux'):
                 file = dirobject + '/' + fnobject
                 remove(file)
-            "If OS not Windows or Linux flag an error"
+            #If OS not Windows or Linux flag an error
             else:
                 raise ValueError('Unknown OS')
 
